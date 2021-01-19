cb41a0a9f1f85b2cf00e80086d1213ed46a97d02
camisbrussi <camila.sbrussi@universo.univates.br>
2021-01-19 20:57:45 -0300

    Project created

---
+++ src/main/java/com/camisbrussi/javaWS/JavaWsApplication.java
@@ -0,0 +1,13 @@
+package com.camisbrussi.javaWS;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaWsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaWsApplication.class, args);
+	}
+
+}
