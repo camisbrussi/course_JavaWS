d8a570794772d441532cbeff9efc34df08f24761
camisbrussi <camila.sbrussi@universo.univates.br>
2021-01-23 21:43:24 -0300

    Exception handling - delete

--- src/main/java/com/camisbrussi/javaWS/services/UserService.java
+++ src/main/java/com/camisbrussi/javaWS/services/UserService.java
@@ -4,11 +4,13 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Component;
-import org.springframework.web.client.ResourceAccessException;
 
 import com.camisbrussi.javaWS.entities.User;
 import com.camisbrussi.javaWS.repositories.UserRepository;
+import com.camisbrussi.javaWS.services.exceptions.DatabaseException;
 import com.camisbrussi.javaWS.services.exceptions.ResourceNotFoundException;
 
 
@@ -33,7 +35,13 @@ public class UserService {
 	}
 	
 	public void delete(Long id) {
+		try {
 		repository.deleteById(id);
+		} catch(EmptyResultDataAccessException e) {
+			throw new ResourceNotFoundException(id);
+		} catch (DataIntegrityViolationException e) {
+			throw new DatabaseException(e.getMessage());
+		}
 	}
 	
 	public User update(Long id, User obj) {
