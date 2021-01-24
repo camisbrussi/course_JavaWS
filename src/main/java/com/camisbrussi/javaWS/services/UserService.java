c92bfe7841222bd7bf3d9dd3a6835c177f97e4ef
camisbrussi <camila.sbrussi@universo.univates.br>
2021-01-23 21:27:35 -0300

    Exception handling - findById

--- src/main/java/com/camisbrussi/javaWS/services/UserService.java
+++ src/main/java/com/camisbrussi/javaWS/services/UserService.java
@@ -5,9 +5,11 @@ import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+import org.springframework.web.client.ResourceAccessException;
 
 import com.camisbrussi.javaWS.entities.User;
 import com.camisbrussi.javaWS.repositories.UserRepository;
+import com.camisbrussi.javaWS.services.exceptions.ResourceNotFoundException;
 
 
 
@@ -23,7 +25,7 @@ public class UserService {
 	
 	public User findById(Long id) {
 		Optional<User> obj = repository.findById(id);
-		return obj.get();
+		return obj.orElseThrow(() -> new ResourceNotFoundException(id));
 	}
 	
 	public User insert(User obj) {
